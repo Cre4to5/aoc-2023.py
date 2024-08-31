e4ff75bfd0dae8c54c997b2cb5a3d1bb9a3323c7
KacperWojas <146821452+Cre4to5@users.noreply.github.com>
2024-08-31 20:54:12 +0200

    day1 part2 done fr this time

--- 2/part1.py
+++ 2/part1.py
@@ -1,14 +1,2 @@
-with open("./1/input.txt") as file:
+with open("./2/input.txt") as file:
     lines = file.readlines()
-
-    numbers = []
-
-    for line in lines:
-
-        digits = []
-
-        for letter in line:
-            if str.isdigit(letter):
-                digits.append(letter)
-        numbers.append(int(digits[0] + digits[-1]))
-    print(sum(numbers))
