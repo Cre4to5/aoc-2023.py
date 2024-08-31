14769a84aeefa8711a12a45d82182867ab1d2a41
KacperWojas <146821452+Cre4to5@users.noreply.github.com>
2024-08-31 20:32:58 +0200

    day1 part2 done

--- 1/part2.py
+++ 1/part2.py
@@ -1,27 +1,47 @@
 with open("./1/input.txt") as file:
     lines = file.readlines()
 
-    string_digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    string_digits = {
+        "one": "1",
+        "1": "1",
+        "two": "2",
+        "2": "2",
+        "three": "3",
+        "3": "3",
+        "four": "4",
+        "4": "4",
+        "five": "5",
+        "5": "5",
+        "six": "6",
+        "6": "6",
+        "seven": "7",
+        "7": "7", 
+        "eight": "8",
+        "8": "8", 
+        "nine": "9",
+        "9": "9"
+    }
 
     numbers = []
 
     for line in lines:
         line = line.strip()
-        print(line)
 
-        for i, string_digit in enumerate(string_digits):#that wont work for overlaping shit like "twone"
-            # print(i)
-            # print(string_digit)
-            line = str.replace(line, string_digit, f"{i + 1}",-1)
+        # print(line)
 
-        print(line)
-
-        digits = []
-        for letter in line:
-            if str.isdigit(letter):
-                digits.append(letter)
+        digits_indexes = {}
+        for string_digit in string_digits.keys():
+            temp = str.find(line, string_digit)
+            if temp != -1:
+                digits_indexes[temp] = string_digit
         
-        numbers.append(int(digits[0] + digits[-1]))
-        print(numbers[-1])
+        # print(digits_indexes)
+
+        digits_indexes_keys = sorted(digits_indexes.keys())
+        
+        # print(digits_indexes_keys[0])
+
+        numbers.append(int(string_digits[digits_indexes[digits_indexes_keys[0]]] + string_digits[digits_indexes[digits_indexes_keys[-1]]]))
+        # print(numbers[-1])
 
     print(sum(numbers))
